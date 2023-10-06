26ca6b7bcdc404023e22e8d8cbbc2f90918f7b39
Heitor Ribeiro <43735246+Heitordrr@users.noreply.github.com>
2023-10-06 19:38:27 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StatusBar } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import Routes from './src/routes'
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <StatusBar backgroundColor="#28A69D" barStyle="light-content"/>
+      <Routes/>
+    </NavigationContainer>    
+  );
+}
+
